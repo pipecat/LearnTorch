6ff44ca7cf8f370b8a37df1f674b732a41547aaa
pipecat <pipecat@yahoo.com>
2020-08-12 01:02:37 +0800

    add AlexNet

--- AlexNet.py
+++ AlexNet.py
@@ -15,12 +15,9 @@ class AlexNet(nn.Module):
             nn.Conv2d(1, 96, 11, 4), # in_channels, out_channels, kernel_size, stride, padding
             nn.ReLU(),
             nn.MaxPool2d(3, 2), # kernel_size, stride
-            # 减小卷积窗口，使用填充为2来使得输入与输出的高和宽一致，且增大输出通道数
             nn.Conv2d(96, 256, 5, 1, 2),
             nn.ReLU(),
             nn.MaxPool2d(3, 2),
-            # 连续3个卷积层，且使用更小的卷积窗口。除了最后的卷积层外，进一步增大了输出通道数。
-            # 前两个卷积层后不使用池化层来减小输入的高和宽
             nn.Conv2d(256, 384, 3, 1, 1),
             nn.ReLU(),
             nn.Conv2d(384, 384, 3, 1, 1),
@@ -29,7 +26,6 @@ class AlexNet(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(3, 2)
         )
-         # 这里全连接层的输出个数比LeNet中的大数倍。使用丢弃层来缓解过拟合
         self.fc = nn.Sequential(
             nn.Linear(256*5*5, 4096),
             nn.ReLU(),
@@ -37,7 +33,6 @@ class AlexNet(nn.Module):
             nn.Linear(4096, 4096),
             nn.ReLU(),
             nn.Dropout(0.5),
-            # 输出层。由于这里使用Fashion-MNIST，所以用类别数为10，而非论文中的1000
             nn.Linear(4096, 10),
         )
 
@@ -48,8 +43,6 @@ class AlexNet(nn.Module):
 
 net = AlexNet()
 print(net)
-
-# 本函数已保存在d2lzh_pytorch包中方便以后使用
 def load_data_fashion_mnist(batch_size, resize=None, root='~/Datasets/FashionMNIST'):
     """Download the fashion mnist dataset and then load into memory."""
     trans = []
@@ -67,7 +60,6 @@ def load_data_fashion_mnist(batch_size, resize=None, root='~/Datasets/FashionMNI
     return train_iter, test_iter
 
 batch_size = 128
-# 如出现“out of memory”的报错信息，可减小batch_size或resize
 train_iter, test_iter = load_data_fashion_mnist(batch_size, resize=224)
 
 lr, num_epochs = 0.001, 5
